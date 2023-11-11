c4e70c4c37d8872246307adeae42af689e86f014
NazmulAlamShawon <shawonkst15@gmail.com>
2023-11-11 17:10:40 +0600

    add rout

---
+++ app/api/mongo/route.js
@@ -0,0 +1,30 @@
+import {MongoClient} from "mongodb"
+
+export async function GET(request){}
+// Replace the uri string with your connection string.
+const uri =
+  "mongodb+srv://<user>:<password>@<cluster-url>?retryWrites=true&writeConcern=majority";
+
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+async function run() {
+  try {
+    await client.connect();
+
+    const database = client.db('sample_mflix');
+    const movies = database.collection('movies');
+
+    // Query for a movie that has the title 'Back to the Future'
+    const query = { title: 'Back to the Future' };
+    const movie = await movies.findOne(query);
+
+    console.log(movie);
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
